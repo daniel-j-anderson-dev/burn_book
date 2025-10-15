d7960e8047f50298bc129e4289758484fc274324
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2025-10-15 17:32:55 -0600

    added image classifier feed forward model description

---
+++ src/image_classifier.rs
@@ -0,0 +1,73 @@
+use burn::{
+    nn::{
+        Dropout, DropoutConfig, Linear, LinearConfig, Relu,
+        conv::{Conv2d, Conv2dConfig},
+        pool::{AdaptiveAvgPool2d, AdaptiveAvgPool2dConfig},
+    },
+    prelude::*,
+};
+
+#[derive(Debug, Config)]
+pub struct ImageClassifierConfiguration {
+    class_count: usize,
+    hidden_layer_size: usize,
+    #[config(default = "0.5")]
+    dropout: f64,
+}
+impl ImageClassifierConfiguration {
+    pub fn init<B: Backend>(&self, device: &B::Device) -> ImageClassifier<B> {
+        ImageClassifier {
+            convolutional_layers: [
+                Conv2dConfig::new([1, 8], [3, 3]).init(device),
+                Conv2dConfig::new([8, 16], [3, 3]).init(device),
+            ],
+            pool: AdaptiveAvgPool2dConfig::new([8, 8]).init(),
+            activation: Relu::new(),
+            linear_layers: [
+                LinearConfig::new(16 * 8 * 8, self.hidden_layer_size).init(device),
+                LinearConfig::new(self.hidden_layer_size, self.class_count).init(device),
+            ],
+            dropout: DropoutConfig::new(self.dropout).init(),
+        }
+    }
+}
+
+/// Our goal will be to create a basic convolutional neural network used for image classification.
+/// - We will keep the model simple by using
+///   - two convolution layers followed
+///   - by two linear layers,
+///   - some pooling
+///   - use dropout to improve training performance
+///   - and some ReLU activations
+#[derive(Debug, Module)]
+pub struct ImageClassifier<B: Backend> {
+    convolutional_layers: [Conv2d<B>; 2],
+    pool: AdaptiveAvgPool2d,
+    dropout: Dropout,
+    linear_layers: [Linear<B>; 2],
+    activation: Relu,
+}
+impl<B: Backend> ImageClassifier<B> {
+    /// # Shape
+    /// - `images`: `[batch_size, row_count, column_count]`
+    /// - return value: `[batch_size, class_count]`
+    pub fn forward(&self, images: Tensor<B, 3>) -> Tensor<B, 2> {
+        let [batch_size, row_count, column_count] = images.dims();
+
+        let x = images.reshape([batch_size, 1, row_count, column_count]);
+
+        let x = self.convolutional_layers[0].forward(x);
+        let x = self.dropout.forward(x);
+        let x = self.convolutional_layers[1].forward(x);
+        let x = self.dropout.forward(x);
+        let x = self.activation.forward(x);
+
+        let x = self.pool.forward(x);
+        let x = x.reshape([batch_size, 16 * 8 * 8]);
+        let x = self.linear_layers[0].forward(x);
+        let x = self.dropout.forward(x);
+        let x = self.activation.forward(x);
+
+        self.linear_layers[1].forward(x)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,19 @@
-fn main() {}
+use burn_book::image_classifier::ImageClassifierConfiguration;
+
+use burn::backend::Wgpu;
+
+type Backend = Wgpu<f32, i32>;
+type Device = <Backend as burn::prelude::Backend>::Device;
+
+const DIGIT_CLASS_COUNT: usize = 10;
+const MNIST_HIDDEN_LAYER_SIZE: usize = 512;
+
+fn main() {
+    let device = Device::default();
+
+    let mnist_classifier =
+        ImageClassifierConfiguration::new(DIGIT_CLASS_COUNT, MNIST_HIDDEN_LAYER_SIZE)
+            .init::<Backend>(&device);
+
+    println!("{}", mnist_classifier)
+}
